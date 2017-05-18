779fe643a59d9febf53b1ea428eaa7b9bbe7c126
Joannis Orlandos <joannis@orlandos.nl>
2017-05-18 16:38:40 +0200

    fix dependency

--- meow.rb
+++ meow.rb
@@ -1,9 +1,9 @@
-class Meow < Formula
+class meow < Formula
   desc "MongoDB Object Persistence Framework without boilerplate code"
   version "0.1.0"
   url "https://github.com/OpenKitten/Meow/releases/download/#{version}/MeowCLI"
 
-  depends_on "MeowSourcery" => :run
+  depends_on "meowsourcery" => :run
 
   def install
     system "mv meowCLI meow"
